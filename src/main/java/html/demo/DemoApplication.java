daddff2f65e397c258bde526cdc0b0c2db6e9946
luckyjd5 <JD210029@reigate.ac.uk>
2022-10-03 15:53:37 +0100

    Initial commit

---
+++ src/main/java/html/demo/DemoApplication.java
@@ -0,0 +1,38 @@
+package html.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@SpringBootApplication
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/HTMLTest") // USE LISTS
+	public String HTMLTest(@RequestParam(value = "details", name = "details", required = false) String[] details, Model model) {
+		model.addAttribute("details", details);
+		return "HTMLTest";
+	}
+
+	@GetMapping("/breakfastMenu")
+	public void breakfastMenu() {
+	}
+	@GetMapping("/kidsMenu")
+	public void kidsMenu() {
+	}
+	@GetMapping("/mainMenu")
+	public void mainMenu() {
+	}
+	@GetMapping("/sundayMenu")
+	public void sundayMenu() {
+	}
+	@GetMapping("/signUp")
+	public void signUp(){
+	}
+}
